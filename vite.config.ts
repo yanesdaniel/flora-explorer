683c5bdf9a646f572cfaca3d6d62e6b8af74d958
Daniel Yanes <danielyanes.eng@gmail.com>
2026-06-26 18:06:43 -0600

    chore: update Vite/TypeScript configuration files
    Vite and TypeScript configuration files updated. Additionally, the
    vite-env.d.ts file was removed because it's no longer needed

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
   resolve: {
-    alias: {
-      '@components': path.resolve(__dirname, './src/components'),
-      '@utils': path.resolve(__dirname, './src/utils')
-    }
+    tsconfigPaths: true
   }
 })
