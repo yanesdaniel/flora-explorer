577a9de5589f8aa3bbf34c1c38c65fde7e57f58b
Daniel Yanes <danielyanes.eng@gmail.com>
2026-06-27 08:41:36 -0600

    refactor: simplify main.tsx structure and update provider usage

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import { ColorModeProvider } from './components/ui/color-mode'
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
+import { Provider } from '@/components/ui/provider'
+import App from './App'
 import './index.css'
 
-const rootElement = document.getElementById('root')
-if (rootElement != null) {
-  const root = createRoot(rootElement)
-  root.render(
-    <StrictMode>
-      <ChakraProvider value={defaultSystem}>
-        <ColorModeProvider>
-          <App />
-        </ColorModeProvider>
-      </ChakraProvider>
-    </StrictMode>
-  )
-} else {
-  console.error('Root element not found')
-}
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Provider>
+      <App />
+    </Provider>
+  </StrictMode>
+)
